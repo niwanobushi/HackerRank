da7924436725435ec1f3803ba8b1bcaa381c0d0c
Rueffbar <nandodebem@hotmail.com>
2020-04-27 00:03:11 -0700

    Add project files.

---
+++ HackerRank/Exercises.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+
+namespace HackerRank
+{
+    public static class Exercises
+    {
+        public static int SockMerchant(int n, int[] ar)
+        {
+            var pairCount = 0;
+            var socksByColor = new Dictionary<int, int>();
+            for (var i = 0; i < n; i++)
+            {
+                var key = ar[i];
+                socksByColor.TryAdd(key, 0);
+                socksByColor[key]++;
+            }
+            foreach (var color in socksByColor)
+            {
+                pairCount += color.Value / 2;
+            }
+            return pairCount;
+        }
+
+        public static int SocketMerchantOnline(int n, int[] ar)
+        {
+            var pairCount = 0;
+            var socksByColor = new Dictionary<int, int>();
+            for (var i = 0; i < n; i++)
+            {
+                var key = ar[i];
+                if (!socksByColor.ContainsKey(key))
+                {
+                    socksByColor.Add(key, 0);
+                }
+                socksByColor[key] = socksByColor[key] + 1;
+            }
+            foreach (var color in socksByColor)
+            {
+                pairCount += color.Value / 2;
+            }
+            return pairCount;
+        }
+
+        public static int CountingValleys(int n, string s)
+        {
+            var valleyCount = 0;
+            var valleySteps = 0;
+            var currentLevel = 0;
+            foreach (var step in s)
+            {
+                currentLevel = step == 'U' ? currentLevel + 1 : currentLevel - 1;
+                if (currentLevel < 0)
+                {
+                    valleySteps++;
+                    if (valleySteps == 1)
+                    {
+                        valleyCount++;
+                    }
+                }
+                else 
+                {
+                    valleySteps = 0;
+                }
+            }
+            return valleyCount;
+        }
+    }
+}
---
+++ HackerRank/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HackerRank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var t = Exercises.SocketMerchantOnline(9, new int[] { 10, 20, 20, 10, 10, 30, 50, 10, 20 });
+            var t = Exercises.CountingValleys(8, "DUDDDUUDUU");
+            Console.WriteLine(t);
+        }
+    }
+}
