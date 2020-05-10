67f6142914e36ad6c55c566c2fb97df0b360a8db
Rueffbar <nandodebem@hotmail.com>
2020-05-10 07:30:35 -0700

    data source path made relative

--- HackerRankTests/Repositories/DataSourceRepository.cs
+++ HackerRankTests/Repositories/DataSourceRepository.cs
@@ -5,10 +5,8 @@ namespace HackerRankTests.Repositories
 {
     public static class DataSourceRepository
     {
-        const string Path = @"C:\Users\nando\Desktop\Estudo\repos\HackerRank\HackerRankTests\Repositories\Data\";
-
         public static string[] GetData(string fileName)
-            => File.ReadAllLines($"{ Path }{fileName}")
+            => File.ReadAllLines($@"{ Path.GetFullPath(@"..\..\..\") }\Repositories\Data\{fileName}")
                    .TakeWhile(line => !string.IsNullOrWhiteSpace(line))
                    .ToArray();
     }
